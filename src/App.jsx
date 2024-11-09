f3b706cbada83d22ff8e6e5230c2e7f92821fe4f
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-11-09 15:37:11 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import Editor from "./pages/Editor";
 import { ToastContainer } from "react-toastify";
