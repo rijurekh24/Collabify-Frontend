46b94ad3eeddd054e0e0576b6495d7a2e6e913e9
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-11-09 23:23:01 +0530

    riju

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,31 @@ import Home from "./pages/Home";
 import Editor from "./pages/Editor";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { Oval } from "react-loader-spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const socket = io("https://collabify-backend.onrender.com");
+
+    socket.on("connect", () => {
+      console.log("Connected to server");
+      setLoading(false);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+      setLoading(true);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <div>
@@ -22,10 +45,24 @@ function App() {
         />
       </div>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/editor/:roomId" element={<Editor />}></Route>
-        </Routes>
+        {loading ? (
+          <div className="loader-container">
+            <Oval
+              height={80}
+              width={80}
+              color="rgba(52, 152, 219, 1)"
+              secondaryColor="rgba(0, 0, 0, 0.1)"
+              ariaLabel="loading"
+              strokeWidth={4}
+              strokeWidthSecondary={4}
+            />
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/editor/:roomId" element={<Editor />} />
+          </Routes>
+        )}
       </Router>
     </>
   );
